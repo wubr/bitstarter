fc9a8cc146c9cf6b43a356dbf0f7c605399ce643
Jacob Ela <jacob.ela@gmail.com>
2013-07-04 15:06:03 -0500

    grader can take web urls

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -45,24 +46,43 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-  $ = cheerioHtmlFile(htmlfile);
+  return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
+};
+
+var checkHtmlWeb = function(url, checksfile) {
+  rest.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+      console.log("error: %s", result.message);
+      process.exit(1);
+    }  else {
+      checkHtml(cheerio.load(result), checksfile)
+    }
+  });
+}
+
+var checkHtml = function(html, checksfile) {
+  $ = html;
   var checks = loadChecks(checksfile).sort();
   var out = {};
   for(var ii in checks) {
     var present = $(checks[ii]).length > 0;
     out[checks[ii]] = present;
   }
-  return out;
+  var outJson = JSON.stringify(out, null, 4);
+  console.log(outJson);
 };
 
 if(require.main == module) {
   program
-    .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-    .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+    .option('-c, --checks <val>', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+    .option('-f, --file <val>', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+    .option('-u, --url <val>', 'URL to load')
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  if (program.url) {
+    checkHtmlWeb(program.url, program.checks);
+  } else {
+    checkHtmlFile(program.file, program.checks);
+  }
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
